111484a7d6af11cab7d607c4240805e4e18ceffb
marcs-feh <marcosfehlauer@protonmail.com>
2026-04-01 22:27:09 -0300

    x

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#include "base.h"
 #include <stdio.h>
 
 int main(){
+    Arena arena = arena_from_virtual(256 * Mem_Gigabyte, 1 * Mem_Megabyte, 4 * Mem_Megabyte);
+
+    u64* p = arena_make(&arena, u64, 730034);
+    printf("%p\n", p);
+
     return 0;
 }
 
