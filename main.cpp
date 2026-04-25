b5e90b04c581a99c74036d21e7b6f0092d2d6964
marcs-feh <marcosfehlauer@protonmail.com>
2026-04-24 22:22:26 -0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -8,37 +8,9 @@
 
 extern "C" int printf(cstring, ...);
 
-template<u32 N>
-void print(BitSet<N> const& s){
-	printf("(%d:%d)[ ", N, s.slot_count);
-	for(usize i = 0; i < N; i += 1){
-		bool b = s.get(i);
-		if(b) printf("\033[0;33m%d\033[0m", int(b));
-		else printf("%d", int(b));
-
-		if(i && ((i+1) % BitSet<N>::slot_width == 0) && (i+1 != N))
-			printf(".");
-	}
-	printf(" ]\n");
-}
-
 int main(){
 	virtual_init();
 	auto allocator = Arena::from_virtual(2 * mem_gigabyte);
-	BitSet<80> set;
-	print(set);
-
-	set.set(3, true);
-	set.set(8, true);
-	set.set(9, true);
-	set.set(15, true);
-	set.set(0, true);
-	set.set(42, true);
-	set.set(69, true);
-	set.set(42, false);
-
-	print(set);
-	print(~set);
 }
 
 //// Single library TU
